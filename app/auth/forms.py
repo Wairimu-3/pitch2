391802490f3df110f4abf4096077206f11f76540
kimanimargaret46@gmail.com <kimanimargaret46@gmail.com>
2020-07-04 14:48:54 +0300

    forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -5,7 +5,7 @@ from ..models import User
 from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[Required(),Email()])
+    email = StringField('Enter your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
     firstname = StringField('Enter your first name',validators = [Required()])
     lastname = StringField('Enter your last name',validators = [Required()])
@@ -15,11 +15,11 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self,data_field):
             if User.query.filter_by(email =data_field.data).first():
-                raise ValidationError('There is an account with that email')
+                raise ValidationError('An account with that name already exists')
 
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
-            raise ValidationError('That username is taken')
+            raise ValidationError('The username you entered is taken')
 
 class LoginForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
