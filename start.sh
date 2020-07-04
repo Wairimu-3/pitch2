8bec2ced72ffd927a4ade87f152a350d8c725111
kimanimargaret46@gmail.com <kimanimargaret46@gmail.com>
2020-07-04 14:40:43 +0300

    manage.py

---
+++ start.sh
@@ -0,0 +1,5 @@
+export SECRET_KEY='2f36c3efdc9a49ab8348612fa4f3fcc4'
+export MAIL_USERNAME='kimanimargaret46@gmail.com'
+export MAIL_PASSWORD='skrtdatway'
+
+python manage.py server
---
+++ try.py
@@ -0,0 +1,2 @@
+from werkzeug import url_encode
+print(1+1)
